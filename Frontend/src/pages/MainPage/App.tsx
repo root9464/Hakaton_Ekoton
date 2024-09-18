67175c858fb65737618c992d637d7effb4e032ae
io <senpai9464@yandex.ru>
2024-09-18 11:52:28 +0000

    cherry pick

--- Frontend/src/pages/MainPage/App.tsx
+++ Frontend/src/pages/MainPage/App.tsx
@@ -4,9 +4,6 @@ import { Menu } from '@/components/Menu/Menu';
 export const App = () => {
   return (
     <>
-      <div className='w-96 h-10 bg-red-500'>
-        <Button text='Button' className='rounded-uiDefault' />
-      </div>
       <Header />
       <Menu />
     </>
