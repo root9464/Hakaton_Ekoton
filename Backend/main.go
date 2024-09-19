7f358e964b35cc2cf75e3426bb379bc48cf28c73
dopamine <ivanbatutin921@mail.ru>
2024-09-19 14:13:32 +0500

    cors

--- Backend/main.go
+++ Backend/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 
 	controllers "root/controllers"
 	db "root/database"
@@ -44,6 +45,9 @@ func NewServer(port string) *Server {
 
 	//s.app.Use(logger.New())
 	s.app.Static("/image", "./image")
+	s.app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+	}))
 
 	return s
 }
