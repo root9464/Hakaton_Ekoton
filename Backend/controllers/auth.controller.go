eabfac24bd0c161afc9ba23d6dc298935f013905
dopamine <ivanbatutin921@mail.ru>
2024-09-19 13:50:46 +0500

    auth number 1312731

--- Backend/controllers/auth.controller.go
+++ Backend/controllers/auth.controller.go
@@ -12,6 +12,7 @@ import (
 func SingUp(c *fiber.Ctx) error {
 	// body struct
 	var body struct {
+		ID        int    `json:"id"`
 		Firstname  string `json:"firstname" gorm:"type:varchar(100)"`
 		Lastname   string `json:"lastname" gorm:"type:varchar(100)"`
 		Patronymic string `json:"patronymic" gorm:"type:varchar(100)"`
@@ -60,12 +61,13 @@ func SingUp(c *fiber.Ctx) error {
 func Login(c *fiber.Ctx) error {
 	// body struct
 	var body struct {
+		ID        int    `json:"id"`
 		Firstname string `json:"firstname"`
 		Lastname  string `json:"lastname"`
 		Patronymic string `json:"patronymic"`
 		Login    string `json:"login"`
 		Password string `json:"password"`
-		//Role     string `json:"role"`
+		Role     string `json:"role"`
 	}
 	if err := c.BodyParser(&body); err != nil {
 		return c.Status(500).JSON(fiber.Map{
